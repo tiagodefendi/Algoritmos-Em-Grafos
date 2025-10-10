13737e4eb37762081eb7a7dc0c141d1c21c1c984
Defendi <tiagodefendidasilva@gmail.com>
2025-10-09 21:29:45 -0300

    Printando o Grafo

--- Atividade Pratica 1/src/DirectedGraph.java
+++ Atividade Pratica 1/src/DirectedGraph.java
@@ -31,11 +31,26 @@ public class DirectedGraph {
         }
         return null;
     }
-
     public void addNode(Node node) {
         this.nodes.add(node);
     }
     public void addEdge(Edge edge) {
         this.edges.add(edge);
+    }
+    public void printGraph() {
+        System.out.println("Graph:");
+        System.out.println(" Node:");
+        for (int i = 1; i < this.nodes.size(); i++) {
+            float id = this.nodes.get(i).getId();
+            String label = this.nodes.get(i).getLabel();
+            System.out.println("    id=" + id + ", label=" + label);
+        }
+        System.out.println(" Edges:");
+        for (int i = 1; i < this.edges.size(); i++) {
+            int id = this.edges.get(i).getId();
+            float source = this.edges.get(i).getSource().getId();
+            float target = this.edges.get(i).getTarget().getId();
+            System.out.println("    id=" + id + ", source=" + source + ", target=" + target);
+        }
     }
 }
--- Atividade Pratica 1/src/Main.java
+++ Atividade Pratica 1/src/Main.java
@@ -2,19 +2,5 @@
 void main() {
     ArrayList<DirectedGraph> graphs = ReadGraphFile.getGraphs("res/LesMiserables.gexf");
     DirectedGraph graph = graphs.get(0);
-
-    System.out.println(graph);
-    System.out.println("Node:");
-    for (int i = 1; i < graph.getNodes().size(); i++) {
-        float id = graph.getNodes().get(i).getId();
-        String label = graph.getNodes().get(i).getLabel();
-        System.out.println("    id=" + id + ", label=" + label);
-    }
-    System.out.println("Edges:");
-    for (int i = 1; i < graph.getEdges().size(); i++) {
-        int id = graph.getEdges().get(i).getId();
-        float source = graph.getEdges().get(i).getSource().getId();
-        float target = graph.getEdges().get(i).getTarget().getId();
-        System.out.println("    id=" + id + ", source=" + source + ", target=" + target);
-    }
+    graph.printGraph();
 }
